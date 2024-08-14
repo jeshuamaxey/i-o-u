786d7cd5487d1974a81a71069fbc42a98b099ac9
Jeshua Maxey <me@jeshua.co>
2024-08-14 12:25:27 +0100

    enact rename of project to IOU

--- apps/webapp/components/Header.tsx
+++ apps/webapp/components/Header.tsx
@@ -24,10 +24,21 @@ export default function Header() {
 
   return (
     <div className="w-full flex flex-col gap-16 items-center border-b border-slate-200">
-      <div className="w-full flex px-4 md:px-8 py-2 gap-8 justify-between">
-        <div className="flex items-center">
-          <p>Split sage</p>
-        </div>
+      <div className="w-full flex px-4 md:px-8 py-2 gap-8 justify-between items-center">
+        <Link href="/groups">
+          <div className="flex items-center group">
+            <p className={`
+              relative transition-all duration-200 ease-in-out
+              font-black tracking-widest top-0 opacity-100
+              group-hover:top-4 group-hover:opacity-0
+              `}>I&nbsp;&nbsp;O&nbsp;&nbsp;U</p>
+            <p className={`
+              relative transition-all duration-200 ease-in-out
+              -top-4 -left-14 opacity-0
+              group-hover:top-0 group-hover:opacity-100
+              `}>👀 😮 🫵</p>
+          </div>
+        </Link>
         <div className="flex justify-end">
           {showAvatar ? (
             <Avatar>
