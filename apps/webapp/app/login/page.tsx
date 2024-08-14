302ec6d047edb2265620a7f9e9edaed4a34f6e3e
Jeshua Maxey <me@jeshua.co>
2024-08-14 11:19:59 +0100

    add login details to make relogin quicker

--- apps/webapp/app/login/page.tsx
+++ apps/webapp/app/login/page.tsx
@@ -82,6 +82,7 @@ export default function Login({
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           name="email"
           placeholder="you@example.com"
+          value="jesh@si.org"
           required
         />
         <label className="text-md" htmlFor="password">
@@ -92,6 +93,7 @@ export default function Login({
           type="password"
           name="password"
           placeholder="••••••••"
+          value="password"
           required
         />
         <SubmitButton
